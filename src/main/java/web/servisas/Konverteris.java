d6591c8695bb38eb668e9702a84f82a99c54d85b
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 00:23:45 +0200

    updated Konverteris.java

--- src/main/java/web/servisas/Konverteris.java
+++ src/main/java/web/servisas/Konverteris.java
@@ -1,44 +1,46 @@
 package web.servisas;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
 
 
-@Path("konverteris")
 public class Konverteris {
+	
+	private static TemperaturuKonteineris tk = new TemperaturuKonteineris(convertFtoC(32));
+	Konverteris() {}
 
-	  @GET
-	  @Produces("application/json")
-	  public Response convertFtoC() throws JSONException {
+	public static Object printTemp(Request request, Response response) throws JSONException {
+ 		return tk.getTemp();	
+	}
+
+ 	public static String postTemp(Request request, Response response) throws JSONException {
+		JSONObject jsonObject = convertFtoC(Double.parseDouble(request.params(":f")));
+		tk.addTemp(jsonObject);
+ 		return "Ok";	
+	}
+
+	public static String putTemp(Request request, Response response) throws JSONException {
+		JSONObject jsonObject = new JSONObject(request.body());
+		JSONObject trueObject = convertFtoC(jsonObject.getDouble("Farenheito reikšmė"));
+		double f = Double.parseDouble(request.params(":f"));
+		tk.upTemp(f, trueObject);
+ 		return "Ok";	
+	}
+
+	public static String deleteTemp(Request request, Response response) throws JSONException {
+		tk.delTemp(Double.parseDouble(request.params(":f")));
+ 		return "Ok";	
+	}
+	
+	public static JSONObject convertFtoC(double f) throws JSONException {
  
 		JSONObject jsonObject = new JSONObject();
-		Double fahrenheit = 98.24;
-		Double celsius;
-		celsius = (fahrenheit - 32)*5/9; 
-		jsonObject.put("Celsijaus reikšmė", celsius);
-		jsonObject.put("Farenheito reikšmė", fahrenheit); 
- 
-		String result = "\n\nF to C konverterio rezultatas: \n\n" + jsonObject;
-		return Response.status(200).entity(result).build();
-	  }
- 
-	  @Path("{f}")
-	  @GET
-	  @Produces("application/json")
-	  public Response convertFtoCfromInput(@PathParam("f") float f) throws JSONException {
- 
-		JSONObject jsonObject = new JSONObject();
-		float celsius;
+		double celsius; 
 		celsius =  (f - 32)*5/9; 
-		jsonObject.put("Celsijaus reikšmė", celsius);
-		jsonObject.put("Farenheito reikšmė", f); 
-		
-		String result = "\n\nF to C konverterio rezultatas: \n\n" + jsonObject;
-		return Response.status(200).entity(result).build();
-	  }
+		jsonObject.put("Farenheito reikšmė", f);
+		jsonObject.put("Celsijaus reikšmė", celsius);	
+		return jsonObject;
+	}
 }

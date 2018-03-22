c2664b006ae78c7361b0d658fa5c7368658c4cb4
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 00:23:00 +0200

    updated Main

--- src/main/java/web/servisas/Main.java
+++ src/main/java/web/servisas/Main.java
@@ -1,36 +1,40 @@
 package web.servisas;
 
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
+import static spark.Spark.*;
 
 import java.io.IOException;
-import java.net.URI;
 
-/**
- * Main class.
- *
- */
+
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:5000/temperatura/";
-
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers in package
-        final ResourceConfig rc = new ResourceConfig().packages("web.servisas");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
-
+   
     public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Temperatūrų programa pasileido "
-                + "\nPaspauskite Enter, kad užbaigtumėte darbą...", BASE_URI));
+	Konverteris k = new Konverteris();
+        port(5000);
+
+        path("/konverteris", () -> {
+
+            get("", (request, response) -> {
+                return k.printTemp(request, response);
+            });
+
+	    post("/:f", (request, response) -> {
+                return k.postTemp(request, response);
+            } , new JsonTransformer());
+		
+            put("/:f", (request, response) -> {
+                return k.putTemp(request, response);
+            }, new JsonTransformer());
+
+            delete("/:f", (request, response) -> {
+                return k.deleteTemp(request, response);
+            }, new JsonTransformer());
+
+        });
+	
+	exception(Exception.class, (e, request, response) -> e.printStackTrace());
+	
+        after((request, response) -> response.type("application/json"));
+
     }
+    
 }

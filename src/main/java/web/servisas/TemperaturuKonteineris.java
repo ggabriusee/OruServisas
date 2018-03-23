e935c72f9d9b8d45249520ffe099fd95c54fb90e
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 08:54:02 +0200

    updated

--- src/main/java/web/servisas/TemperaturuKonteineris.java
+++ src/main/java/web/servisas/TemperaturuKonteineris.java
@@ -12,24 +12,37 @@ public class TemperaturuKonteineris{
 		jsonArray.add(jsonObj);
 	}
 	
-	public void delTemp(double f){
+	public void delTemp(double f) throws JSONException, Exception{
+		boolean check=false;
 		for (int i = 0; i < jsonArray.size(); i++) {
 			if(jsonArray.get(i).getDouble("Farenheito reikšmė") == f){
+				check=true;
 				jsonArray.remove(i);
+				break;
 			}
 		}
+		if(check==false){throw new Exception("Tokios reikšmės nėra");}
 	}
 	
-	public void upTemp(double f, JSONObject jsonObj){
+	public void upTemp(double f, JSONObject jsonObj) throws JSONException, Exception {
+		boolean check=false;
 		for (int i = 0; i < jsonArray.size(); i++) {
 			if(jsonArray.get(i).getDouble("Farenheito reikšmė") == f){
+				check=true;
 				jsonArray.get(i).put("Farenheito reikšmė", jsonObj.getDouble("Farenheito reikšmė"));
 				jsonArray.get(i).put("Celsijaus reikšmė", jsonObj.getDouble("Celsijaus reikšmė"));
+				break;
 			}
 		}
+		if(check==false){throw new Exception("Tokios reikšmės nėra");}
 	}
 
-	public void addTemp(JSONObject jsonObj){
+	public void addTemp(JSONObject jsonObj) throws JSONException, Exception {
+		for (int i = 0; i < jsonArray.size(); i++) {
+			if(jsonArray.get(i).getDouble("Farenheito reikšmė") == jsonObj.getDouble("Farenheito reikšmė")){
+				throw new Exception("Jau yra tokia reikšmė");
+			}
+		}
 		jsonArray.add(jsonObj);
 	}
 	public ArrayList<JSONObject> getTemp(){

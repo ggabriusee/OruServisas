5b1e374a2945da6f29419f38ddf920ef4afef725
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 11:41:44 +0200

    up

--- src/main/java/web/servisas/Konverteris.java
+++ src/main/java/web/servisas/Konverteris.java
@@ -10,46 +10,56 @@ import spark.Response;
 public class Konverteris {
 
     	private static final int HTTP_NOT_FOUND = 404;
-	private static final int HTTP_CONFLICT = 409;
-	private static TemperaturuKonteineris tk = new TemperaturuKonteineris(convertFtoC(32));
+	//private static final int HTTP_CONFLICT = 409;
+	private static TemperaturuKonteineris tk = new TemperaturuKonteineris();
 	Konverteris() {}
 
-	public static Object printTemp(Request request, Response response) throws JSONException {
+	public static Object printTemp(Request request, Response response) {
  		return tk.getTemp();	
 	}
 
- 	public static String postTemp(Request request, Response response) throws JSONException {
+ 	public static String postTemp(Request request, Response response) {
 		try{
-			JSONObject jsonObject = convertFtoC(Double.parseDouble(request.params(":f")));
-			tk.addTemp(jsonObject);
+			CityData cityObject = JsonTransformer.fromJson(request.body(), CityData.class);
+			tk.addTemp(cityObject);
  		return "Ok";
 		} catch (Exception e) {
-			response.status(HTTP_CONFLICT);
-		    	return "Saraše jau yra " + request.params(":f") + " reikšmė";
+			response.status(HTTP_NOT_FOUND);
+		    	return "Blogai nurodytas formatas";
         	}	
 	}
 
-	public static String putTemp(Request request, Response response) throws JSONException {
+	public static String putTemp(Request request, Response response) {
 		try{
-			JSONObject jsonObject = new JSONObject(request.body());
-			JSONObject trueObject = convertFtoC(jsonObject.getDouble("Farenheito reikšmė"));
-			double f = Double.parseDouble(request.params(":f"));
-			tk.upTemp(f, trueObject);
+			CityData cityObject = JsonTransformer.fromJson(request.body(), CityData.class);
+            		int id = Integer.valueOf(request.params(":f"));
+			tk.upTemp(id, cityObject);
  			return "Ok";
 		} catch (Exception e) {
 			response.status(HTTP_NOT_FOUND);
-		    	return "Nepavyko rasti " + request.params(":f") + " reikšmės";
+		    	return "Nepavyko rasti duomenų su " + request.params(":f") + " id";
         	}	
 	}
 
-	public static String deleteTemp(Request request, Response response) throws JSONException {
+	public static String deleteTemp(Request request, Response response) {
 		try{
-			tk.delTemp(Double.parseDouble(request.params(":f")));
+			int id = Integer.valueOf(request.params(":f"));
+			tk.delTemp(Integer.valueOf(request.params(":f")));
 	 		return "Ok";
 		} catch (Exception e) {
 			response.status(HTTP_NOT_FOUND);
-		    	return "Nepavyko rasti " + request.params(":f") + " reikšmės";
+		    	return "Nepavyko rasti duomenų su " + request.params(":f") + " id";
 		}	
+	}
+
+	public static Object getTempSpecific(Request request, Response response){
+		try{
+			return tk.getTempSpec(request.params(":f"));
+		} catch (Exception e) {
+			response.status(HTTP_NOT_FOUND);
+		    	return "Nepavyko rasti duomenų su " + request.params(":f") + " id";
+		}	
+
 	}
 	
 	public static JSONObject convertFtoC(double f) throws JSONException {

1604a1ebc4f7109c32cb5d85088e0fda07e5ebde
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 11:41:18 +0200

    up

--- src/main/java/web/servisas/JsonTransformer.java
+++ src/main/java/web/servisas/JsonTransformer.java
@@ -3,13 +3,21 @@ package web.servisas;
 import com.google.gson.Gson;
 import spark.ResponseTransformer;
 
+
 public class JsonTransformer implements ResponseTransformer {
 
-    private Gson gson = new Gson();
+    private static Gson gson = new Gson();
 
     @Override
     public String render(Object model) {
         return gson.toJson(model);
     }
+	
+	public static <T extends Object> T  fromJson(String json, Class<T> classe) {
+        return gson.fromJson(json, classe);
+    }
+
+
+	
 
 }

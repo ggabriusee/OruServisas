309ddbc108445ffb64fe4759588b552a41e1b974
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 11:42:50 +0200

    up

--- src/main/java/web/servisas/TemperaturuKonteineris.java
+++ src/main/java/web/servisas/TemperaturuKonteineris.java
@@ -6,57 +6,58 @@ import java.util.ArrayList;
 
 public class TemperaturuKonteineris{
 
-	private ArrayList<JSONObject> jsonArray = new ArrayList<JSONObject>();
+	private ArrayList<CityData> cityArray = new ArrayList<CityData>();
 
-	TemperaturuKonteineris(JSONObject jsonObj){
-		jsonArray.add(jsonObj);
+	TemperaturuKonteineris(){
+		cityArray.add(new CityData(1, 0, "Vilnius", "2018-02-02"));
+		cityArray.add(new CityData(2, 15, "Kaunas", "2018-04-02"));
+		cityArray.add(new CityData(3, -20, "Vilnius", "2018-12-30"));
 	}
 	
-	public void delTemp(double f) throws JSONException, Exception{
+	public void delTemp(int id) throws Exception{
 		boolean check=false;
-		for (int i = 0; i < jsonArray.size(); i++) {
-			if(jsonArray.get(i).getDouble("Farenheito reikšmė") == f){
+		for (int i = 0; i < cityArray.size(); i++) {
+			if(cityArray.get(i).getId() == id){
 				check=true;
-				jsonArray.remove(i);
+				cityArray.remove(i);
 				break;
 			}
 		}
 		if(check==false){throw new Exception("Tokios reikšmės nėra");}
 	}
 	
-	public void upTemp(double f, JSONObject jsonObj) throws JSONException, Exception {
+	public void upTemp(int f, CityData cityObj) throws Exception {
 		boolean check=false;
-		for (int i = 0; i < jsonArray.size(); i++) {
-			if(jsonArray.get(i).getDouble("Farenheito reikšmė") == f){
+		for (int i = 0; i < cityArray.size(); i++) {
+			if(cityArray.get(i).getId() == f){
 				check=true;
-				jsonArray.get(i).put("Farenheito reikšmė", jsonObj.getDouble("Farenheito reikšmė"));
-				jsonArray.get(i).put("Celsijaus reikšmė", jsonObj.getDouble("Celsijaus reikšmė"));
+				cityArray.get(i).setTemperature(cityObj.getTemperature());
+				cityArray.get(i).setCity(cityObj.getCity());
+				cityArray.get(i).setDate(cityObj.getDate());
 				break;
 			}
 		}
 		if(check==false){throw new Exception("Tokios reikšmės nėra");}
 	}
 
-	public void addTemp(JSONObject jsonObj) throws JSONException, Exception {
-		for (int i = 0; i < jsonArray.size(); i++) {
-			if(jsonArray.get(i).getDouble("Farenheito reikšmė") == jsonObj.getDouble("Farenheito reikšmė")){
-				throw new Exception("Jau yra tokia reikšmė");
+	public void addTemp(CityData cityObj)  {
+		cityObj.setId(cityArray.get(cityArray.size()-1).getId()+1);
+		cityArray.add(cityObj);
+	}
+	public ArrayList<CityData> getTemp(){
+		return cityArray;
+	}
+	public Object getTempSpec(String s) throws Exception {
+		boolean check=false;
+		ArrayList<CityData> cityMas = new ArrayList<CityData>();
+		for (int i = 0; i < cityArray.size(); i++) {
+			if(cityArray.get(i).getCity().equals(s)){
+				check=true;
+				cityMas.add(cityArray.get(i));
 			}
 		}
-		jsonArray.add(jsonObj);
+		if(check==false){throw new Exception("Tokios reikšmės nėra");}
+		return cityMas;
 	}
-	public ArrayList<JSONObject> getTemp(){
-		return jsonArray;
-	}
-
-
-
-
-
-
-
-
-
-
 
 }

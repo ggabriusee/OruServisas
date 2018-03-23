dfcc19c1e97b693c1fd3de03e3b5a72ceaf5de44
ggabriusee <36458983+ggabriusee@users.noreply.github.com>
2018-03-23 08:55:11 +0200

    updated

--- src/main/java/web/servisas/Konverteris.java
+++ src/main/java/web/servisas/Konverteris.java
@@ -6,8 +6,10 @@ import spark.Request;
 import spark.Response;
 
 
+
 public class Konverteris {
-	
+
+    	private static final int HTTP_NOT_FOUND = 404;
 	private static TemperaturuKonteineris tk = new TemperaturuKonteineris(convertFtoC(32));
 	Konverteris() {}
 
@@ -16,22 +18,37 @@ public class Konverteris {
 	}
 
  	public static String postTemp(Request request, Response response) throws JSONException {
-		JSONObject jsonObject = convertFtoC(Double.parseDouble(request.params(":f")));
-		tk.addTemp(jsonObject);
- 		return "Ok";	
+		try{
+			JSONObject jsonObject = convertFtoC(Double.parseDouble(request.params(":f")));
+			tk.addTemp(jsonObject);
+ 		return "Ok";
+		} catch (Exception e) {
+			response.status(HTTP_NOT_FOUND);
+		    	return "Saraše jau yra " + request.params(":f") + " reikšmė";
+        	}	
 	}
 
 	public static String putTemp(Request request, Response response) throws JSONException {
-		JSONObject jsonObject = new JSONObject(request.body());
-		JSONObject trueObject = convertFtoC(jsonObject.getDouble("Farenheito reikšmė"));
-		double f = Double.parseDouble(request.params(":f"));
-		tk.upTemp(f, trueObject);
- 		return "Ok";	
+		try{
+			JSONObject jsonObject = new JSONObject(request.body());
+			JSONObject trueObject = convertFtoC(jsonObject.getDouble("Farenheito reikšmė"));
+			double f = Double.parseDouble(request.params(":f"));
+			tk.upTemp(f, trueObject);
+ 			return "Ok";
+		} catch (Exception e) {
+			response.status(HTTP_NOT_FOUND);
+		    	return "Nepavyko rasti " + request.params(":f") + " reikšmės";
+        	}	
 	}
 
 	public static String deleteTemp(Request request, Response response) throws JSONException {
-		tk.delTemp(Double.parseDouble(request.params(":f")));
- 		return "Ok";	
+		try{
+			tk.delTemp(Double.parseDouble(request.params(":f")));
+	 		return "Ok";
+		} catch (Exception e) {
+			response.status(HTTP_NOT_FOUND);
+		    	return "Nepavyko rasti " + request.params(":f") + " reikšmės";
+		}	
 	}
 	
 	public static JSONObject convertFtoC(double f) throws JSONException {
